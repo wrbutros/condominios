5dcf22b7a7bb1354a038dbadcddb7b811ef55548
Wilson Bustos <wrbutros@gmail.com>
2015-11-13 12:11:56 -0300

    se agregan comuna y ciudad a la administracion

--- registro/models.py
+++ registro/models.py
@@ -254,7 +254,20 @@ class AdministradorEdificioAdmin(admin.ModelAdmin):
     get_nombre_ciudad.admin_order_field  = 'ciudad'  #Allows column order sorting
     get_nombre_ciudad.short_description  = 'Ciudad'  #Renames column head
 
+class CiudadAdmin(admin.ModelAdmin):
+    model=Ciudad
+    list_display=('nombre',)
+    list_filter=('nombre',) #hay que filtrar ciudad
+    search_fields=('nombre',)
 
+class ComunaAdmin(admin.ModelAdmin):
+    model=Comuna
+    list_display=('nombre', 'get_nombre_ciudad')
+    list_filter=('nombre',) #hay que filtrar ciudad
+    search_fields=('nombre', 'get_nombre_ciudad')
+
+    def get_nombre_ciudad(self, obj):
+        return obj.ciudad.nombre
 
 
 admin.site.register(Residente, ResidenteAdmin)
@@ -264,3 +277,5 @@ admin.site.register(Edificio, EdificioAdmin)
 admin.site.register(Conserje, ConserjeAdmin)
 admin.site.register(Contrato, ContratoAdmin)
 admin.site.register(AdministradorEdificio, AdministradorEdificioAdmin)
+admin.site.register(Ciudad, CiudadAdmin)
+admin.site.register(Comuna, ComunaAdmin)
