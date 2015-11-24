8fff799a1acd88bff4734623074f4fe06720fc73
David Daniel Portella de la Cruz <david@davidportella.cl>
2015-11-24 17:17:58 -0300

    Docker support and multi settings file

---
+++ condominios/settings/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from local import *
+except ImportError as e:
+    from defaults import *
---
+++ condominios/settings/devel.py
@@ -0,0 +1,13 @@
+from defaults import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+ENVIRONMENT = 'devel'
