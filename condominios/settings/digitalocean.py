d9a45e8f0a9f4d7f590d5a23cbdede31c30ff3a0
David Portella <david@davidportella.cl>
2016-04-21 17:11:17 -0300

    feat(deploy) - Digital Ocean settings

--- condominios/settings/digitalocean.py
+++ condominios/settings/digitalocean.py
@@ -2,9 +2,17 @@ from defaults import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'condominios',
+        'USER': 'django',
+        'PASSWORD': 'condominios',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
-ENVIRONMENT = 'prod'
+ENVIRONMENT = 'devel'
+
+CORS_ORIGIN_WHITELIST = (
+    'condominio-transparente.dyndns.org'
+)
