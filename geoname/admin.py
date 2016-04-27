190c2ac203f7fbd9159852d4db72eac4f5018024
Wilson Bustos <wrbutros@gmail.com>
2016-04-27 14:23:12 -0300

    feat(geoname) - Add entities to administration panel

--- geoname/admin.py
+++ geoname/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Ciudad, Comuna
 
-# Register your models here.
+ ##  ====== CIUDAD =======
+ class CiudadAdmin(admin.ModelAdmin):
+     model = Ciudad
+     list_display = ('nombre',)
+     list_filter = ('nombre',)  # hay que filtrar ciudad
+     search_fields = ('nombre',)
+
+
+ ##  ====== COMUNA =======
+ class ComunaAdmin(admin.ModelAdmin):
+     model = Comuna
+     list_display = ('nombre', 'get_nombre_ciudad')
+     list_filter = ('nombre',)  # hay que filtrar ciudad
+     search_fields = ('nombre', 'get_nombre_ciudad')
+
+     def get_nombre_ciudad(self, obj):
+         return obj.ciudad.nombre
+
+admin.site.register(Ciudad, CiudadAdmin)
+admin.site.register(Comuna, ComunaAdmin)
