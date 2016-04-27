871dafc64fb85c9efc95bee0bafa07632e070fc0
David Portella <david@davidportella.cl>
2016-04-27 15:37:39 -0300

    fix(geoname) - fix ident in admin

--- geoname/admin.py
+++ geoname/admin.py
@@ -1,23 +1,24 @@
 from django.contrib import admin
-from .models import Ciudad, Comuna
-
- ##  ====== CIUDAD =======
- class CiudadAdmin(admin.ModelAdmin):
-     model = Ciudad
-     list_display = ('nombre',)
-     list_filter = ('nombre',)  # hay que filtrar ciudad
-     search_fields = ('nombre',)
+from .models import Ciudad, Comuna  ##  ====== CIUDAD =======
 
 
- ##  ====== COMUNA =======
- class ComunaAdmin(admin.ModelAdmin):
-     model = Comuna
-     list_display = ('nombre', 'get_nombre_ciudad')
-     list_filter = ('nombre',)  # hay que filtrar ciudad
-     search_fields = ('nombre', 'get_nombre_ciudad')
+class CiudadAdmin(admin.ModelAdmin):
+    model = Ciudad
+    list_display = ('nombre',)
+    list_filter = ('nombre',)  # hay que filtrar ciudad
+    search_fields = ('nombre',)
 
-     def get_nombre_ciudad(self, obj):
-         return obj.ciudad.nombre
+
+##  ====== COMUNA =======
+class ComunaAdmin(admin.ModelAdmin):
+    model = Comuna
+    list_display = ('nombre', 'get_nombre_ciudad')
+    list_filter = ('nombre',)  # hay que filtrar ciudad
+    search_fields = ('nombre', 'get_nombre_ciudad')
+
+    def get_nombre_ciudad(self, obj):
+        return obj.ciudad.nombre
+
 
 admin.site.register(Ciudad, CiudadAdmin)
 admin.site.register(Comuna, ComunaAdmin)
